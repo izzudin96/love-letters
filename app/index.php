fe4cf27143b5686c68f53453a122f285cddf5176
Izzudin Anuar <izzudinanuar96@gmail.com>
2021-11-24 21:43:25 +1300

    Exclude vendor

--- app/index.php
+++ app/index.php
@@ -4,6 +4,10 @@ require __DIR__.'/vendor/autoload.php';
 
 use App\Commands\EmailQuoteCommand;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
+
+$env = new Dotenv();
+$env->load(__DIR__.'/.env');
 
 $application  = new Application();
 
